31c7868d30027eb8da3025458f7214adb9f0aa4c
OH_HONGKEUN <OH_HONGKEUN@eland.co.kr>
2025-04-02 15:57:44 +0900

    feat: init project

---
+++ src/main/kotlin/org/srunners/msa/user/Application.kt
@@ -0,0 +1,11 @@
+package org.srunners.msa.user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
